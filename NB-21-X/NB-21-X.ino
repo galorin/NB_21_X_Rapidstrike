9bee2e198125fa3f5a8f64e028f0af3ea3a1dbdf
Richard Smith <r.smith@jamesjones.co.uk>
2016-01-11 14:10:00 +0000

    Added in a basic sketch that will only do display and hard-coded full auto firing.  Stripped out previous sketch in its entirety.

--- NB-21-X/NB-21-X.ino
+++ NB-21-X/NB-21-X.ino
@@ -2,58 +2,58 @@
 
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+//#include <Adafruit_GFX.h>
+//#include <Adafruit_SSD1306.h>
 
 // If using software SPI (the default case):
-#define OLED_MOSI   9
-#define OLED_CLK   10
-#define OLED_DC    11
-#define OLED_CS    12
-#define OLED_RESET 13
-Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+//#define OLED_MOSI   9
+//#define OLED_CLK   10
+//#define OLED_DC    11
+//#define OLED_CS    12
+//#define OLED_RESET 13
+//Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 #define FLYWHEEL_FET  19 //A6
 #define PUSHER_FET 22 //A7
 
-#define BUTTON1 23 //A0
-#define BUTTON2 24 //A1
-#define BUTTON3 25 //A2
+//#define BUTTON1 23 //A0
+//#define BUTTON2 24 //A1
+//#define BUTTON3 25 //A2
 
 #define REV_TRIGGER 32 //D2
 #define FIRE_TRIGGER 1 //D3
-#define JAM_DOOR 2     //D4
+//#define JAM_DOOR 2     //D4
 #define PUSHER 9 //D5
-#define DART_SENSOR 10 //D6
-#define MAG_SENSOR 11 //D7
+//#define DART_SENSOR 10 //D6
+//#define MAG_SENSOR 11 //D7
 
 Bounce revTrigger = Bounce();
 Bounce fireTrigger = Bounce();
-Bounce jamDoor = Bounce();
+//Bounce jamDoor = Bounce();
 Bounce pusher = Bounce();
-Bounce magSensor = Bounce();
-Bounce but1 = Bounce();
-Bounce but2 = Bounce();
-Bounce but3 = Bounce();
+//Bounce magSensor = Bounce();
+//Bounce but1 = Bounce();
+//Bounce but2 = Bounce();
+//Bounce but3 = Bounce();
 
-bool blinkOn = true;
-bool fDartSeen = true;
+//bool blinkOn = true;
+//bool fDartSeen = true;
 
-int flyPercent = 0;
-int pushPercent = 0;
+//int flyPercent = 0;
+//int pushPercent = 0;
 
-#if (SSD1306_LCDHEIGHT != 64)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
+//#if (SSD1306_LCDHEIGHT != 64)
+//#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+//#endif
 
 void setup()   {                
   Serial.begin(9600);
   
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-  display.begin(SSD1306_SWITCHCAPVCC);
+//  display.begin(SSD1306_SWITCHCAPVCC);
   // init done  
-  display.clearDisplay();
-  display.setTextColor(WHITE);
+//  display.clearDisplay();
+//  display.setTextColor(WHITE);
 
   pinMode(REV_TRIGGER, INPUT_PULLUP);
   revTrigger.attach(REV_TRIGGER);
@@ -67,40 +67,40 @@ void setup()   {
   pusher.attach(PUSHER);
   pusher.interval(5);
 
-  pinMode(JAM_DOOR, INPUT_PULLUP);  
-  jamDoor.attach(JAM_DOOR);
-  jamDoor.interval(5);
+  //pinMode(JAM_DOOR, INPUT_PULLUP);  
+  //jamDoor.attach(JAM_DOOR);
+  //jamDoor.interval(5);
 
-  pinMode(BUTTON1, INPUT_PULLUP);
-  but1.attach(BUTTON1);
-  but1.interval(5);
+  //pinMode(BUTTON1, INPUT_PULLUP);
+  //but1.attach(BUTTON1);
+  //but1.interval(5);
   
-  pinMode(BUTTON2, INPUT_PULLUP);
-  but2.attach(BUTTON2);
-  but2.interval(5);
+  //pinMode(BUTTON2, INPUT_PULLUP);
+  //but2.attach(BUTTON2);
+  //but2.interval(5);
   
-  pinMode(BUTTON3, INPUT_PULLUP);
-  but3.attach(BUTTON3);
-  but3.interval(5);
+  //pinMode(BUTTON3, INPUT_PULLUP);
+  //but3.attach(BUTTON3);
+  //but3.interval(5);
 
-  pinMode(DART_SENSOR, INPUT_PULLUP);
+  //pinMode(DART_SENSOR, INPUT_PULLUP);
 }
 
 void loop() {
  revTrigger.update();
  fireTrigger.update();
  pusher.update();
- jamDoor.update();
- but1.update();
- but2.update();
- but3.update();
+ //jamDoor.update();
+ //but1.update();
+ //but2.update();
+ //but3.update();
 
  int revOn = revTrigger.read();
  int fireOn = fireTrigger.read();
  int pusherOn = pusher.read();
- int jamDoorOn = jamDoor.read();
- int but1On = but1.read();
- int but2On = but2.read();
- int but3On = but3.read();
+ //int jamDoorOn = jamDoor.read();
+ //int but1On = but1.read();
+ //int but2On = but2.read();
+ //int but3On = but3.read();
 
 }
